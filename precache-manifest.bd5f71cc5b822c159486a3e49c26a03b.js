d259c24553dd0a6a45446d62882a424b51af2bb7
thouime <ouimetom@gmail.com>
2023-02-16 12:44:29 -0700

    Updates

---
+++ precache-manifest.bd5f71cc5b822c159486a3e49c26a03b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa1f92480fbff20d30b0",
+    "url": "/portfolio/static/css/main.c1792d8f.chunk.css"
+  },
+  {
+    "revision": "aa1f92480fbff20d30b0",
+    "url": "/portfolio/static/js/main.aa1f9248.chunk.js"
+  },
+  {
+    "revision": "b3a284bf9cca557d1785",
+    "url": "/portfolio/static/js/1.b3a284bf.chunk.js"
+  },
+  {
+    "revision": "df65aa3be63f71e84109",
+    "url": "/portfolio/static/js/2.df65aa3b.chunk.js"
+  },
+  {
+    "revision": "4f79856aba4398cdc5b0",
+    "url": "/portfolio/static/js/runtime~main.4f79856a.js"
+  },
+  {
+    "revision": "f727b25366484ce385174a50d56bb629",
+    "url": "/portfolio/static/media/css.f727b253.svg"
+  },
+  {
+    "revision": "db24418ed88ab248fcbfef63826301c8",
+    "url": "/portfolio/static/media/figma.db24418e.svg"
+  },
+  {
+    "revision": "8fc6417dadf8c6beadf3ccd1ef79e8e1",
+    "url": "/portfolio/static/media/html.8fc6417d.svg"
+  },
+  {
+    "revision": "a672c2a10891cf0bff437c0938fd7986",
+    "url": "/portfolio/static/media/javascript.a672c2a1.svg"
+  },
+  {
+    "revision": "d8f3fced44dab1f3da5d306698216342",
+    "url": "/portfolio/static/media/react.d8f3fced.svg"
+  },
+  {
+    "revision": "e3afd5c25d0e3012f0ac2ed936560d69",
+    "url": "/portfolio/static/media/background-light.e3afd5c2.svg"
+  },
+  {
+    "revision": "c72cf4789fd62e7b45e8983eb2120b38",
+    "url": "/portfolio/static/media/background.c72cf478.svg"
+  },
+  {
+    "revision": "28817bb294877459f2222a1aa4ce06b7",
+    "url": "/portfolio/static/media/factory.28817bb2.svg"
+  },
+  {
+    "revision": "8d4fd02dcd157cfe1d44cc7a2e8df5df",
+    "url": "/portfolio/static/media/forms.8d4fd02d.svg"
+  },
+  {
+    "revision": "b76dda1be5da3a74acac388a37a5ecc6",
+    "url": "/portfolio/static/media/programming.b76dda1b.svg"
+  },
+  {
+    "revision": "31cc7ef458984e33558c807146120ebc",
+    "url": "/portfolio/static/media/quiz.31cc7ef4.svg"
+  },
+  {
+    "revision": "e61880a1c674e6596c8750694f90f40e",
+    "url": "/portfolio/static/media/terms.e61880a1.svg"
+  },
+  {
+    "revision": "1149c16db2f4cf3679e0644906e1d1b7",
+    "url": "/portfolio/static/media/thought.1149c16d.svg"
+  },
+  {
+    "revision": "61e80fcb598cd184aefe7611acac3698",
+    "url": "/portfolio/static/media/docpage.1c01c2d3.gif"
+  },
+  {
+    "revision": "9c13b627e885e308614eddebe0bdb392",
+    "url": "/portfolio/static/media/productpage.ecac24a4.gif"
+  },
+  {
+    "revision": "b4d86c6274b0d869d4634fde6ce14612",
+    "url": "/portfolio/static/media/quizzical.9df1f51b.gif"
+  },
+  {
+    "revision": "3364b5570482832dd02946d9a579c455",
+    "url": "/portfolio/static/media/surveyform.a6793a22.gif"
+  },
+  {
+    "revision": "e8d4a2b2b269c75ff2300329481697c7",
+    "url": "/portfolio/static/media/tributepage.1a9b6ec0.png"
+  },
+  {
+    "revision": "c0c9bfbd78c41bd2c48caecd16586f03",
+    "url": "/portfolio/index.html"
+  }
+];

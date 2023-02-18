75e588755b64d82a115594ee58ae15dfcae4c521
thouime <ouimetom@gmail.com>
2023-02-18 14:03:35 -0700

    Cleaned up code

--- src/components/Project.js
+++ src/components/Project.js
@@ -49,86 +49,59 @@ export default function Project() {
     //     </Link>
     // );
     
-    const projectList = projects.map((project, index) => 
-        project.react ? (
+    const projectList = projects.map((project, index) => {
+        const projectCards = (
+            <div className="project--card">
+                <div className={`project--front ${index % 2 !== 0 && "reverse"}`}>
+                    <img
+                        className="project--img"
+                        alt="gallery"
+                        src={require(`../images/${project.image}`).default}
+                        key={`../images/${project.image}`}
+                    />
+                    <img
+                        className="project--svg"
+                        alt="man writing on large document"
+                        src={svgDir(`./${project.svg}`)}
+                        key={svgDir(`./${project.svg}`)}
+                    />
+                </div>
+                <div className={`project--back ${index % 2 === 0 && "reverse"}`}>
+                    <div className="project--skills">
+                        {project.skills.map((skill => (
+                            <img
+                                alt="svg"
+                                src={svgDir(`./${skill + ".svg"}`)}
+                                key={svgDir(`./${skill + ".svg"}`)}
+                            />
+                        )))}
+                    </div>
+                    <div className="project--text">
+                        <h1>{project.title}</h1>
+                        <p>{project.description}</p>
+                    </div>
+                </div>
+            </div>
+        );
+        return project.react ? (
             <Link 
-            to={ project.link }
-            key={`../images/${project.image}`}
-            target="_blank"
-            >
-            <div className="project--card">
-                <div className={`project--front ${index%2 !== 0 && "reverse"}`}>
-                <img
-                    className="project--img"
-                    alt="gallery"
-                    src={require(`../images/${project.image}`).default}
-                    key={`../images/${project.image}`}
-                />
-                <img 
-                    className="project--svg"
-                    alt="man writing on large document"
-                    src={svgDir(`./${project.svg}`)}
-                    key={svgDir(`./${project.svg}`)}
-                />
-                </div>
-                <div className={`project--back ${index%2 === 0 && "reverse"}`}>
-                <div className="project--skills">
-                    {project.skills.map((skill => 
-                    <img
-                        alt="svg"
-                        src={svgDir(`./${skill + ".svg"}`)}
-                        key={svgDir(`./${skill + ".svg"}`)}
-                    />
-                    ))}
-                </div>
-                <div className="project--text">
-                    <h1>{project.title}</h1>
-                    <p>{project.description}</p>
-                </div>
-                </div>
-            </div>
+                to={ project.link }
+                key={`../images/${project.image}`}
+                target="_blank"
+                >
+                {projectCards}
             </Link>
-        ): (
+        ) : (
             <a 
-            href={`../../projects/${project.link}`} 
-            key={`../images/${project.image}`} 
-            target="_blank" 
-            rel="noopener noreferrer"
+                href={`../../projects/${project.link}`} 
+                key={`../images/${project.image}`} 
+                target="_blank" 
+                rel="noopener noreferrer"
             >
-            <div className="project--card">
-                <div className={`project--front ${index%2 !== 0 && "reverse"}`}>
-                <img
-                    className="project--img"
-                    alt="gallery"
-                    src={require(`../images/${project.image}`).default}
-                    key={`../images/${project.image}`}
-                />
-                <img 
-                    className="project--svg"
-                    alt="man writing on large document"
-                    src={svgDir(`./${project.svg}`)}
-                    key={svgDir(`./${project.svg}`)}
-                />
-                </div>
-                <div className={`project--back ${index%2 === 0 && "reverse"}`}>
-                <div className="project--skills">
-                    {project.skills.map((skill => 
-                    <img
-                        alt="svg"
-                        src={svgDir(`./${skill + ".svg"}`)}
-                        key={svgDir(`./${skill + ".svg"}`)}
-                    />
-                    ))}
-                </div>
-                <div className="project--text">
-                    <h1>{project.title}</h1>
-                    <p>{project.description}</p>
-                </div>
-                </div>
-            </div>
+                {projectCards}
             </a>
         )
-        );
+    });
 
     return (
         <section id="project">

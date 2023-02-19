e086b8f83c5735ce19547da3e95dc6894c1c9dc6
thouime <ouimetom@gmail.com>
2023-02-19 11:07:06 -0700

    Reverted back to basename for routing

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import QuizzicalApp from "./components/Quizzical/components/QuizzicalApp";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/quizzical" element={<QuizzicalApp />} />

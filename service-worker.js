5578c06dc8647c17663465e3a5fafa97dd65e4fb
thouime <ouimetom@gmail.com>
2023-02-19 11:07:41 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ce8255b944a4ee2a24d2a6d227661208.js"
+  "/portfolio/precache-manifest.9f00a901b5f02372b6bdd77e67b209ae.js"
 );
 
 workbox.clientsClaim();
